9d421e58113af516a015587d114ae7391c78941e
Zoran Jankov <zoran.jankov.87@gmail.com>
2021-03-27 15:44:55 +0100

    Update Rename-Computers-Tool.ps1

--- Rename-Computers-Tool.ps1
+++ Rename-Computers-Tool.ps1
@@ -11,54 +11,59 @@ Version:        1.0
 Author:         Zoran Jankov
 #>
 
+$SearchBase = "OU=Korisnici,OU=Centrala,DC=uni,DC=net"
+
 Import-Module "$PSScriptRoot\Modules\Write-Log.psm1"
-$Credential = Get-Credential
 $CSVFile = $null
 
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName PresentationFramework
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
-$RenameComputersForm             = New-Object system.Windows.Forms.Form
-$RenameComputersForm.ClientSize  = New-Object System.Drawing.Point(669,301)
-$RenameComputersForm.Text        = "Rename Computers Tool"
-$RenameComputersForm.TopMost     = $true
+$MainForm                        = New-Object system.Windows.Forms.Form
+$MainForm.ClientSize             = New-Object System.Drawing.Point(670,295)
+$MainForm.Text                   = "Rename Computers Tool"
+$MainForm.TopMost                = $true
+$MainForm.FormBorderStyle        = 'Fixed3D'
+$MainForm.MaximizeBox            = $false
+$MainForm.ShowIcon               = $false
 
-$OldNameLabel                    = New-Object system.Windows.Forms.Label
-$OldNameLabel.Text               = "Old Name"
-$OldNameLabel.AutoSize           = $true
-$OldNameLabel.Width              = 25
-$OldNameLabel.Height             = 10
-$OldNameLabel.Location           = New-Object System.Drawing.Point(25,70)
-$OldNameLabel.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$ComputerNameLabel               = New-Object system.Windows.Forms.Label
+$ComputerNameLabel.Text          = "Computer"
+$ComputerNameLabel.AutoSize      = $true
+$ComputerNameLabel.Width         = 25
+$ComputerNameLabel.Height        = 10
+$ComputerNameLabel.Location      = New-Object System.Drawing.Point(25,70)
+$ComputerNameLabel.Font          = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $NewNameLabel                    = New-Object system.Windows.Forms.Label
 $NewNameLabel.Text               = "New Name"
 $NewNameLabel.AutoSize           = $true
 $NewNameLabel.Width              = 25
 $NewNameLabel.Height             = 10
-$NewNameLabel.Location           = New-Object System.Drawing.Point(25,105)
+$NewNameLabel.Location           = New-Object System.Drawing.Point(25,110)
 $NewNameLabel.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$OldNameTextBox                  = New-Object system.Windows.Forms.TextBox
-$OldNameTextBox.Multiline        = $false
-$OldNameTextBox.Width            = 377
-$OldNameTextBox.Height           = 25
-$OldNameTextBox.Location         = New-Object System.Drawing.Point(117,64)
-$OldNameTextBox.Font             = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$ComputerNameComboBox            = New-Object system.Windows.Forms.ComboBox
+$ComputerNameComboBox.Width      = 380
+$ComputerNameComboBox.Height     = 25
+$ComputerNameComboBox.Location   = New-Object System.Drawing.Point(120,65)
+$ComputerNameComboBox.Font       = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$ComputerNameComboBox.AutoCompleteMode = 'SuggestAppend'
+$ComputerNameComboBox.AutoCompleteSource = 'ListItems'
 
 $NewNameTextBox                  = New-Object system.Windows.Forms.TextBox
 $NewNameTextBox.Multiline        = $false
-$NewNameTextBox.Width            = 377
+$NewNameTextBox.Width            = 380
 $NewNameTextBox.Height           = 25
-$NewNameTextBox.Location         = New-Object System.Drawing.Point(117,100)
+$NewNameTextBox.Location         = New-Object System.Drawing.Point(120,105)
 $NewNameTextBox.Font             = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $SingleRenameButton              = New-Object system.Windows.Forms.Button
 $SingleRenameButton.Text         = "Rename"
-$SingleRenameButton.Width        = 114
+$SingleRenameButton.Width        = 115
 $SingleRenameButton.Height       = 30
-$SingleRenameButton.Location     = New-Object System.Drawing.Point(527,76)
+$SingleRenameButton.Location     = New-Object System.Drawing.Point(525,80)
 $SingleRenameButton.Font         = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $SingleRenameLabel               = New-Object system.Windows.Forms.Label
@@ -66,7 +71,7 @@ $SingleRenameLabel.Text          = "Single Rename"
 $SingleRenameLabel.AutoSize      = $true
 $SingleRenameLabel.Width         = 25
 $SingleRenameLabel.Height        = 10
-$SingleRenameLabel.Location      = New-Object System.Drawing.Point(279,21)
+$SingleRenameLabel.Location      = New-Object System.Drawing.Point(280,25)
 $SingleRenameLabel.Font          = New-Object System.Drawing.Font('Microsoft Sans Serif',10,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
 
 $BulkRenameLabel                 = New-Object system.Windows.Forms.Label
@@ -74,7 +79,7 @@ $BulkRenameLabel.Text            = "Bulk Rename"
 $BulkRenameLabel.AutoSize        = $true
 $BulkRenameLabel.Width           = 25
 $BulkRenameLabel.Height          = 10
-$BulkRenameLabel.Location        = New-Object System.Drawing.Point(287,145)
+$BulkRenameLabel.Location        = New-Object System.Drawing.Point(280,145)
 $BulkRenameLabel.Font            = New-Object System.Drawing.Font('Microsoft Sans Serif',10,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
 
 $CSVFileLabel                    = New-Object system.Windows.Forms.Label
@@ -82,7 +87,7 @@ $CSVFileLabel.Text               = "CSV File"
 $CSVFileLabel.AutoSize           = $true
 $CSVFileLabel.Width              = 25
 $CSVFileLabel.Height             = 10
-$CSVFileLabel.Location           = New-Object System.Drawing.Point(25,197)
+$CSVFileLabel.Location           = New-Object System.Drawing.Point(25,210)
 $CSVFileLabel.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $FilePathTextBox                 = New-Object system.Windows.Forms.TextBox
@@ -90,30 +95,47 @@ $FilePathTextBox.Multiline       = $false
 $FilePathTextBox.Width           = 377
 $FilePathTextBox.Height          = 25
 $FilePathTextBox.Enabled         = $false
-$FilePathTextBox.Location        = New-Object System.Drawing.Point(116,191)
+$FilePathTextBox.Location        = New-Object System.Drawing.Point(115,205)
 $FilePathTextBox.Font            = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $SelectFileButton1               = New-Object system.Windows.Forms.Button
 $SelectFileButton1.Text          = "Select File"
-$SelectFileButton1.Width         = 114
+$SelectFileButton1.Width         = 120
 $SelectFileButton1.Height        = 30
-$SelectFileButton1.Location      = New-Object System.Drawing.Point(527,184)
+$SelectFileButton1.Location      = New-Object System.Drawing.Point(525,200)
 $SelectFileButton1.Font          = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $BulkRenameButton                = New-Object system.Windows.Forms.Button
 $BulkRenameButton.Text           = "Bulk Rename"
-$BulkRenameButton.Width          = 156
+$BulkRenameButton.Width          = 160
 $BulkRenameButton.Height         = 30
-$BulkRenameButton.Location       = New-Object System.Drawing.Point(256,241)
+$BulkRenameButton.Location       = New-Object System.Drawing.Point(250,250)
 $BulkRenameButton.Font           = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$RenameComputersForm.controls.AddRange(@($OldNameLabel,$NewNameLabel,$OldNameTextBox,$NewNameTextBox,$SingleRenameButton,$SingleRenameLabel,$BulkRenameLabel,$CSVFileLabel,$FilePathTextBox,$SelectFileButton1,$BulkRenameButton))
+$MainForm.controls.AddRange(@(
+    $ComputerNameLabel,
+    $NewNameLabel,
+    $ComputerNameComboBox,
+    $NewNameTextBox,
+    $SingleRenameButton,
+    $SingleRenameLabel,
+    $BulkRenameLabel,
+    $CSVFileLabel,
+    $FilePathTextBox,
+    $SelectFileButton1,
+    $BulkRenameButton
+))
+
+$Computers = Get-ADComputer -Filter {Enabled -eq $true} -SearchBase $SearchBase | Sort-Object Name
+foreach ($Computer in $Computers) {
+    $ComputerNameComboBox.Items.Add($Computer.Name);
+}
 
 $SingleRenameButton.Add_Click({
-    $OldName = $OldNameTextBox.text
+    $OldName = $ComputerNameComboBox.text
     $NewName = $NewNameTextBox.text
     try {
-        Rename-Computer -ComputerName $OldName -NewName $NewName -DomainCredential $Credential
+        Rename-Computer -ComputerName $OldName -NewName $NewName
     }
     catch {
         $Message ="Faild to rename $OldName computer `n" + $_.Exception
@@ -135,7 +157,7 @@ $SelectFileButton1.Add_Click({
 $BulkRenameButton.Add_Click({
     foreach ($Computer in $CSVFile) {
         try {
-            Rename-Computer -ComputerName $OldName -NewName $NewName -DomainCredential $Credential
+            Rename-Computer -ComputerName $OldName -NewName $NewName
         }
         catch {
             $Message ="Faild to rename " + $Computer.OldName + " computer `n" + $_.Exception
@@ -146,4 +168,4 @@ $BulkRenameButton.Add_Click({
     }
 })
 
-[void]$RenameComputersForm.ShowDialog()
+[void]$MainForm.ShowDialog()

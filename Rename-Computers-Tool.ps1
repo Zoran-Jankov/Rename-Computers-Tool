c146b1c85195c210071fb91008214a2a0101c3f1
Zoran Jankov <zoran.jankov.87@gmail.com>
2021-03-28 18:48:28 +0200

    Update

--- Rename-Computers-Tool.ps1
+++ Rename-Computers-Tool.ps1
@@ -1,32 +1,128 @@
 <#
 .SYNOPSIS
-PowerShell Tool for renaming network computers with graphical user interface.
+PowerShell Tool for renaming AD computers with graphical user interface.
 
 .DESCRIPTION
 PowerShell Tool for renaming network computers with graphical user interface that can be used to rename single computer or to
-perform bulk rename drawing data from a `.csv` file. The `.csv` file must have OldName and NewName columns for tool to work.
+perform bulk rename drawing data from a `.csv` file. The `.csv` file must have ComputerName and NewName columns for tool to work.
 
 .NOTES
-Version:        1.0
+Version:        1.1
 Author:         Zoran Jankov
 #>
 
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
 $SearchBase = "OU=Korisnici,OU=Centrala,DC=uni,DC=net"
 
-Import-Module "$PSScriptRoot\Modules\Write-Log.psm1"
-$CSVFile = $null
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+<#
+.SYNOPSIS
+Writes a log entry to console, log file and report file.
+
+.DESCRIPTION
+Creates a log entry with timestamp and message passed thru a parameter Message, and saves the log entry to log file. Depending on
+the NoTimestamp parameter, log entry can be written with or without a timestamp. Format of the timestamp is
+"yyyy.MM.dd. HH:mm:ss:fff", and this function adds " - " after timestamp and before the main message.
+
+.PARAMETER Message
+A string message to be written as a log entry
+
+.PARAMETER NoTimestamp
+A switch parameter if present timestamp is disabled in log entry
+
+.EXAMPLE
+Write-Log -Message "A log entry"
+
+.EXAMPLE
+Write-Log "A log entry"
+
+.EXAMPLE
+Write-Log -Message "===========" -NoTimestamp
+
+.EXAMPLE
+"A log entry" | Write-Log
+
+.NOTES
+Version:        2.2
+Author:         Zoran Jankov
+#>
+function Write-Log {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true,
+                   Position = 0,
+                   ValueFromPipeline = $true,
+                   ValueFromPipelineByPropertyName = $true,
+                   HelpMessage = "A string message to be written as a log entry")]
+        [string]
+        $Message,
+
+        [Parameter(Mandatory = $false,
+                   Position = 1,
+                   ValueFromPipeline = $true,
+                   ValueFromPipelineByPropertyName = $true,
+                   HelpMessage = "A switch parameter if present timestamp is disabled in log entry")]
+        [switch]
+        $NoTimestamp = $false
+    )
+
+    begin {
+        $Desktop = [Environment]::GetFolderPath("Desktop")
+        $LogFile = "$Desktop\Log.log"
+        if (-not (Test-Path -Path $LogFile)) {
+            New-Item -Path $LogFile -ItemType File
+        }
+    }
+
+    process {
+        if (-not($NoTimestamp)) {
+            $Timestamp = Get-Date -Format "yyyy.MM.dd. HH:mm:ss:fff"
+            $LogEntry = "$Timestamp - $Message"
+        }
+        else {
+            $LogEntry = $Message
+        }
+        Add-content -Path $LogFile -Value $LogEntry
+    }
+}
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
 
 Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName PresentationFramework
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 $MainForm                        = New-Object system.Windows.Forms.Form
-$MainForm.ClientSize             = New-Object System.Drawing.Point(670,295)
+$MainForm.ClientSize             = New-Object System.Drawing.Point(670,300)
 $MainForm.Text                   = "Rename Computers Tool"
 $MainForm.TopMost                = $true
 $MainForm.FormBorderStyle        = 'Fixed3D'
 $MainForm.MaximizeBox            = $false
 $MainForm.ShowIcon               = $false
+$MainForm.StartPosition          = [System.Windows.Forms.FormStartPosition]::CenterScreen
+$MainForm.ForeColor              = "#FFFFFF"
+$MainForm.BackColor              = "#1167B1"
+$MainForm.Font                   = New-Object System.Drawing.Font(
+    'Microsoft Sans Serif',11)
+
+$SingleRenameLabel               = New-Object system.Windows.Forms.Label
+$SingleRenameLabel.Text          = "Single Rename"
+$SingleRenameLabel.AutoSize      = $false
+$SingleRenameLabel.Width         = 670
+$SingleRenameLabel.Height        = 35
+$SingleRenameLabel.Location      = New-Object System.Drawing.Point(0,0)
+$SingleRenameLabel.Font                   = New-Object System.Drawing.Font(
+    'Microsoft Sans Serif',15)
+$SingleRenameLabel.TextAlign = "MiddleCenter"
+$SingleRenameLabel.BackColor = "#3D3D3D"
+
+$BulkRenameLabel                 = New-Object system.Windows.Forms.Label
+$BulkRenameLabel.Text            = "Bulk Rename"
+$BulkRenameLabel.AutoSize        = $true
+$BulkRenameLabel.Width           = 25
+$BulkRenameLabel.Height          = 10
+$BulkRenameLabel.Location        = New-Object System.Drawing.Point(285,165)
 
 $ComputerNameLabel               = New-Object system.Windows.Forms.Label
 $ComputerNameLabel.Text          = "Computer"
@@ -34,7 +130,6 @@ $ComputerNameLabel.AutoSize      = $true
 $ComputerNameLabel.Width         = 25
 $ComputerNameLabel.Height        = 10
 $ComputerNameLabel.Location      = New-Object System.Drawing.Point(25,70)
-$ComputerNameLabel.Font          = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $NewNameLabel                    = New-Object system.Windows.Forms.Label
 $NewNameLabel.Text               = "New Name"
@@ -42,13 +137,11 @@ $NewNameLabel.AutoSize           = $true
 $NewNameLabel.Width              = 25
 $NewNameLabel.Height             = 10
 $NewNameLabel.Location           = New-Object System.Drawing.Point(25,110)
-$NewNameLabel.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $ComputerNameComboBox            = New-Object system.Windows.Forms.ComboBox
 $ComputerNameComboBox.Width      = 380
 $ComputerNameComboBox.Height     = 25
-$ComputerNameComboBox.Location   = New-Object System.Drawing.Point(120,65)
-$ComputerNameComboBox.Font       = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$ComputerNameComboBox.Location   = New-Object System.Drawing.Point(115,65)
 $ComputerNameComboBox.AutoCompleteMode = 'SuggestAppend'
 $ComputerNameComboBox.AutoCompleteSource = 'ListItems'
 
@@ -56,31 +149,8 @@ $NewNameTextBox                  = New-Object system.Windows.Forms.TextBox
 $NewNameTextBox.Multiline        = $false
 $NewNameTextBox.Width            = 380
 $NewNameTextBox.Height           = 25
-$NewNameTextBox.Location         = New-Object System.Drawing.Point(120,105)
-$NewNameTextBox.Font             = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
-
-$SingleRenameButton              = New-Object system.Windows.Forms.Button
-$SingleRenameButton.Text         = "Rename"
-$SingleRenameButton.Width        = 115
-$SingleRenameButton.Height       = 30
-$SingleRenameButton.Location     = New-Object System.Drawing.Point(525,80)
-$SingleRenameButton.Font         = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
-
-$SingleRenameLabel               = New-Object system.Windows.Forms.Label
-$SingleRenameLabel.Text          = "Single Rename"
-$SingleRenameLabel.AutoSize      = $true
-$SingleRenameLabel.Width         = 25
-$SingleRenameLabel.Height        = 10
-$SingleRenameLabel.Location      = New-Object System.Drawing.Point(280,25)
-$SingleRenameLabel.Font          = New-Object System.Drawing.Font('Microsoft Sans Serif',10,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
-
-$BulkRenameLabel                 = New-Object system.Windows.Forms.Label
-$BulkRenameLabel.Text            = "Bulk Rename"
-$BulkRenameLabel.AutoSize        = $true
-$BulkRenameLabel.Width           = 25
-$BulkRenameLabel.Height          = 10
-$BulkRenameLabel.Location        = New-Object System.Drawing.Point(280,145)
-$BulkRenameLabel.Font            = New-Object System.Drawing.Font('Microsoft Sans Serif',10,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+$NewNameTextBox.Location         = New-Object System.Drawing.Point(115,105)
+$NewNameTextBox.MaxLength        = 15
 
 $CSVFileLabel                    = New-Object system.Windows.Forms.Label
 $CSVFileLabel.Text               = "CSV File"
@@ -88,29 +158,32 @@ $CSVFileLabel.AutoSize           = $true
 $CSVFileLabel.Width              = 25
 $CSVFileLabel.Height             = 10
 $CSVFileLabel.Location           = New-Object System.Drawing.Point(25,210)
-$CSVFileLabel.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $FilePathTextBox                 = New-Object system.Windows.Forms.TextBox
 $FilePathTextBox.Multiline       = $false
-$FilePathTextBox.Width           = 377
+$FilePathTextBox.Width           = 380
 $FilePathTextBox.Height          = 25
 $FilePathTextBox.Enabled         = $false
 $FilePathTextBox.Location        = New-Object System.Drawing.Point(115,205)
-$FilePathTextBox.Font            = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$SelectFileButton1               = New-Object system.Windows.Forms.Button
-$SelectFileButton1.Text          = "Select File"
-$SelectFileButton1.Width         = 120
-$SelectFileButton1.Height        = 30
-$SelectFileButton1.Location      = New-Object System.Drawing.Point(525,200)
-$SelectFileButton1.Font          = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$SelectFileButton               = New-Object system.Windows.Forms.Button
+$SelectFileButton.Text          = "Select File"
+$SelectFileButton.Width         = 120
+$SelectFileButton.Height        = 30
+$SelectFileButton.Location      = New-Object System.Drawing.Point(525,200)
+$SelectFileButton.BackColor = "#3D3D3D"
+
+$SingleRenameButton              = New-Object system.Windows.Forms.Button
+$SingleRenameButton.Text         = "Rename"
+$SingleRenameButton.Width        = 115
+$SingleRenameButton.Height       = 30
+$SingleRenameButton.Location     = New-Object System.Drawing.Point(525,80)
 
 $BulkRenameButton                = New-Object system.Windows.Forms.Button
 $BulkRenameButton.Text           = "Bulk Rename"
 $BulkRenameButton.Width          = 160
 $BulkRenameButton.Height         = 30
 $BulkRenameButton.Location       = New-Object System.Drawing.Point(250,250)
-$BulkRenameButton.Font           = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $MainForm.controls.AddRange(@(
     $ComputerNameLabel,
@@ -122,11 +195,11 @@ $MainForm.controls.AddRange(@(
     $BulkRenameLabel,
     $CSVFileLabel,
     $FilePathTextBox,
-    $SelectFileButton1,
+    $SelectFileButton,
     $BulkRenameButton
 ))
 
-$Computers = Get-ADComputer -Filter {Enabled -eq $true} -SearchBase $SearchBase | Sort-Object Name
+$Computers = Get-ADComputer -Filter {Enabled -eq $true} -SearchBase $SearchBase | Sort-Object 'Name'
 foreach ($Computer in $Computers) {
     $ComputerNameComboBox.Items.Add($Computer.Name);
 }
@@ -134,38 +207,72 @@ foreach ($Computer in $Computers) {
 $SingleRenameButton.Add_Click({
     $OldName = $ComputerNameComboBox.text
     $NewName = $NewNameTextBox.text
-    try {
+    $Message = "Are you sure you want to rename '$OldName' computer to '$NewName'?"
+    $Choice =  [System.Windows.Forms.MessageBox]::Show(
+        $Message, "Rename Computer", 
+        [System.Windows.Forms.MessageBoxButtons]::YesNo,
+        [System.Windows.Forms.MessageBoxIcon]::Question
+    )
+    if ($Choice -eq 'Yes') {
         Rename-Computer -ComputerName $OldName -NewName $NewName
+        Get-ADComputer -Identity $NewName
+        if ($?) {
+            $Message = "Successfully renamed '$OldName' computer to '$NewName'"
+            $Title = "Info"
+            $Icon = [System.Windows.Forms.MessageBoxIcon]::Asterisk
+        }
+        else {
+            $Message ="Faild to rename '$OldName' computer"
+            $Title = "Error"
+            $Icon = [System.Windows.Forms.MessageBoxIcon]::Error
+        }
+        Write-Log -Message $Message
+        [System.Windows.Forms.MessageBox]::Show(
+            $Message,
+            $Title,
+            [System.Windows.Forms.MessageBoxButtons]::OK,
+            $Icon
+        )
     }
-    catch {
-        $Message ="Faild to rename $OldName computer `n" + $_.Exception
-        break
-    }
-    $Message = "Successfully renamed " + $OldName + " computer to " + $NewName
-    Write-Log -Message $Message
-    [System.Windows.MessageBox]::Show($Message)
 })
-$SelectFileButton1.Add_Click({
+
+$SelectFileButton.Add_Click({
     $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
     $OpenFileDialog.InitialDirectory = [Environment]::GetFolderPath("Desktop")
     $OpenFileDialog.Filter = "CSV Files (*.csv)| *.csv*"
     $OpenFileDialog.ShowDialog() | Out-Null
-    $CSVFile = Get-Content -Path $OpenFileDialog.Filename
     $FilePathTextBox.Text = $OpenFileDialog.Filename
 })
 
 $BulkRenameButton.Add_Click({
-    foreach ($Computer in $CSVFile) {
-        try {
+    $Message = "Are you sure you want to rename '$OldName' computer to '$NewName'?"
+    $Choice =  [System.Windows.Forms.MessageBox]::Show(
+        $Message, "Rename Computer", 
+        [System.Windows.Forms.MessageBoxButtons]::YesNo,
+        [System.Windows.Forms.MessageBoxIcon]::Question
+    )
+    if ($Choice -eq 'Yes') {
+        $CSVFile = Get-Content -Path $OpenFileDialog.Filename
+        $Info = ""
+        foreach ($Computer in $CSVFile) {
+            $OldName = $Computer.ComputerName
+            $NewName = $Computer.NewName
             Rename-Computer -ComputerName $OldName -NewName $NewName
+            Get-ADComputer -Identity $NewName
+            if ($?) {
+                $Message = "Successfully renamed '$OldName' computer to '$NewName'"
+            }
+            else {
+                $Message ="Faild to rename '$OldName' computer"
+            }
+            Write-Log -Message $Message
+            $Info += "$Message`r`n"
+            [System.Windows.Forms.MessageBox]::Show(
+                $Info, "Info",
+                [System.Windows.Forms.MessageBoxButtons]::OK,
+                [System.Windows.Forms.MessageBoxIcon]::Asterisk
+            )
         }
-        catch {
-            $Message ="Faild to rename " + $Computer.OldName + " computer `n" + $_.Exception
-            continue
-        }
-        $Message = "Successfully renamed " + $Computer.OldName + " computer to " + $Computer.NewName
-        Write-Log -Message $Message
     }
 })
-
 [void]$MainForm.ShowDialog()
